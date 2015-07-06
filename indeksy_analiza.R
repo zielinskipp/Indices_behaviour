81a9a4d6649c7421b962080ed8018c522259d31c
zielinskipp <greenryuninja@gmail.com>
2015-07-07 00:24:52 +0200

    średnia ruchoma i wyliczone indeksy

--- indeksy_analiza.R
+++ indeksy_analiza.R
@@ -15,7 +15,12 @@ sound <- readWave(paste0(directory, sounds[1]), 1, 60, "seconds")
 sound_af <- afilter(sound, plot = F, output = "Wave")
 
 sound_af10 <- afilter(sound, plot = T, threshold = 10, output = "Wave")
-sound_am <-stats::filter(as.matrix(sound),filter = c(1,1,1)/3) 
+sound_am <-stats::filter(as.vector(sound@left),filter = c(1,1,1)/3) 
+sound_am <- Wave(as.vector(sound_am))
+
+naa <- which(is.na(sound_am@left))
+
+sound_am@left<- sound_am@left[-naa]
 
 sound_bp <- ffilter(sound, from = 1000, to = 13000, output = "Wave")
 sound_lp <- ffilter(sound, to = 16000, output = "Wave")
@@ -31,21 +36,20 @@ writeWave(sound_bp, "output/sound_bp.wav")
 writeWave(sound_lp, "output/sound_lp.wav")
 writeWave(sound_lp_af, "output/sound_lp_af.wav")
 writeWave(sound_bp_af, "output/sound_bp_af.wav")
+writeWave(sound_am, "output/sound_am.wav")
+
+
+spectro(sound_am)
 
 
 
-spectro(sound)
-
-
-
-dane_raw <- htrgnt_sound(sound)
-dane_af <- htrgnt_sound(sound_af)
 dane_af10 <- htrgnt_sound(sound_af10)
 dane_bp <- htrgnt_sound(sound_bp)
 dane_lp <- htrgnt_sound(sound_lp)
 dane_bp <-htrgnt_sound(sound_bp)
 dane_lp_af <- htrgnt_sound(sound_lp_af)
 dane_bp_af <-htrgnt_sound(sound_bp_af)
+dane_am <- htrgnt_sound(sound_am)
 
 dane_whole <- rbind(dane_raw, dane_af, dane_af10, dane)
 spectro(sound_af)
